ad9b31940a4856eb2df4b0cb6c15df479adede17
Adrian Menendez <admenva@gmail.com>
2020-01-31 21:08:50 +0000

    Initial commit

---
+++ Assets/ApplicationManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenu;
+
+    public enum GameStatus
+    {
+        InMenu,
+        Playing,
+        GameOver
+    }
+    public GameStatus currentGameStatus = GameStatus.InMenu;
+
+    public void StartGame()
+    {
+        //TODO Hide the Menu,
+        //Spawn the Player,
+        //Let the LevelManager Know what to do.
+        mainMenu.SetActive(currentGameStatus == GameStatus.InMenu);
+        Time.timeScale = 1;
+
+        currentGameStatus = GameStatus.Playing;
+    }
+
+    public void PauseGame()
+    {
+        mainMenu.SetActive(currentGameStatus == GameStatus.InMenu);
+        Time.timeScale = 0;
+    }
+
+    public void EndGame()
+    {
+        // IF we have time ADD Score
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioSource menuMusic;
+
+    [SerializeField]
+    AudioSource playMusic1;
+    [SerializeField]
+    AudioSource playMusic2;
+
+
+    //TODO Add audio Effects
+    List<AudioSource> allAudioSources = new List<AudioSource>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allAudioSources.Add(menuMusic);
+        allAudioSources.Add(playMusic1);
+        allAudioSources.Add(playMusic2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
