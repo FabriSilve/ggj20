d6db433c21fcfa3f5c79d1ffe6fe7665b1925546
Adrian Menendez <admenva@gmail.com>
2020-01-31 22:37:16 +0000

    Basic HUD

---
+++ Assets/Scripts/HudManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ship;
+
+    [SerializeField]
+    float roundTime; // Time to reach goal (in seconds)
+
+    [SerializeField]
+    int startingPosition;
+
+    [SerializeField]
+    int endingPosition;
+
+    float elapsedTime = 0;
+
+    void Update()
+    {
+        if (elapsedTime < roundTime && ship.transform.position.x < endingPosition)
+        {
+            elapsedTime += Time.deltaTime;
+            float newPos = (endingPosition - startingPosition) * elapsedTime / roundTime;
+            ship.transform.position = new Vector3(newPos, ship.transform.position.y, ship.transform.position.z);
+        }
+    }
+}
