09c1b68a26ff71d0244fe565e5f4ef43c2cf0344
Abraham Georgiadis <abraham@manomotion.com>
2020-02-01 18:39:33 +0100

    Made Changes on the Buy, now you can buy stuff and have the items grey out if you cant afford them.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,26 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Item : MonoBehaviour
 {
+    public static Action<Item> OnPurchaseMade;
+
     public int ID;
     public int weight = 1;
     public int price = 1;
+
+    [SerializeField]
+    Button myButton;
+
+    bool hasSubscribedToEvent;
+
+
+    void HandleCreditUpdated(int credit)
+    {
+        Debug.Log("Handling the event from button " + this.gameObject.name);
+
+        myButton.interactable = credit >= price;
+
+    }
+
+
 
     public void Interact()
     {
 
     }
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
+        if (!myButton)
+        {
+            myButton = GetComponent<Button>();
+        }
+
+        if (myButton)
+        {
+            if (Wallet.Instance)
+            {
+                if (!hasSubscribedToEvent)
+                {
+                    Wallet.OnCreditUpdated += HandleCreditUpdated;
+                }
+                myButton.interactable = Wallet.Instance.Credit() >= this.price;
+            }
+
+        }
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+
+    public void BuyThisItem()
+    {
+        if (OnPurchaseMade != null)
+        {
+            OnPurchaseMade(this);
+
+        }
     }
 }
--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -1,41 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class Wallet : MonoBehaviour
 {
+    public static Action<int> OnCreditUpdated;
+    private static Wallet _instance;
+    public static Wallet Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            Item.OnPurchaseMade += HandleItemPurchase;
+
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void HandleItemPurchase(Item newItem)
+    {
+        credit -= newItem.price;
+        if (OnCreditUpdated != null)
+        {
+            OnCreditUpdated(credit);
+        }
+        else
+        {
+            Debug.Log("Noone is listening");
+        }
+        Debug.LogFormat("I purchased item with id {0} for price {1} my remaining money is {2}", newItem.ID, newItem.price, credit);
+    }
     [SerializeField]
     private int credit = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public int Credit() {
+    public int Credit()
+    {
         return credit;
     }
 
-    public void AddCredit(int amount) {
+    public void AddCredit(int amount)
+    {
         credit += amount;
     }
 
-    public bool CanWithdraw(int amount) {
+    public bool CanWithdraw(int amount)
+    {
         return amount <= credit;
     }
 
-    public bool Withdraw(int amount) {
-        if (CanWithdraw(amount)) {
+    public bool Withdraw(int amount)
+    {
+        if (CanWithdraw(amount))
+        {
             credit -= amount;
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
