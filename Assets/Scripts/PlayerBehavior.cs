32ae1340cc65226ab70068dd7c36a5c949a62f7f
Abraham Georgiadis <abraham@manomotion.com>
2020-02-01 12:06:52 +0100

    My first commit. Brought the scripts from local to repo. About to work on Spawn Points

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public float speed = 50f;
+    public enum ControlMode
+    {
+        SinglePlayer,
+        CoOp
+    }
+
+    public ControlMode currentcontrolMode;
+
+
+    public float movementSpeed = 50f;
 
     private CharacterController controller;
 
@@ -14,27 +23,104 @@ public class PlayerBehavior : MonoBehaviour
 
     void Interact()
     {
-        if (currentItem)
-        {
-            currentItem.Interact();
-        }
+        //if (currentItem)
+        //{
+        //    currentItem.Interact();
+        //}
     }
 
 
     void ControlPlayer()
     {
+        //TODO we can use Kinnematic is is to avoid the gravity.
+
         gravity = controller.isGrounded ? 0 : gravity - (9.81f * Time.deltaTime);
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), gravity, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * speed);
+        controller.Move(move * Time.deltaTime * movementSpeed);
+    }
+
+    void ControlPlayerWithArrows()
+    {
+        switch (currentcontrolMode)
+        {
+            case ControlMode.SinglePlayer:
+                //TODO use some sort of controls
+                //TODO Use for now the Keyboard
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    this.transform.position += transform.right * -1;
+                    //Rotate Left
+                }
+                else if (Input.GetKeyDown(KeyCode.E))
+                {
+                    this.transform.position += transform.right;
+
+                    //Rotate Right
+
+                }
+                else if (Input.GetKeyDown(KeyCode.W))
+                {
+                    this.transform.position += transform.forward;
+
+                    //Move Forward
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    this.transform.position += transform.forward * -1;
+
+                    //Move Backwards
+
+                }
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Interact
+                }
+
+
+
+
+                break;
+            case ControlMode.CoOp:
+                //TODO do the same but split the parts.
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    //Rotate Left
+                }
+                else if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //Rotate Right
+
+                }
+                else if (Input.GetKeyDown(KeyCode.W))
+                {
+                    //Move Forward
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    //Move Backwards
+
+                }
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Interact
+                    Interact();
+                }
+
+                break;
+            default:
+                break;
+        }
     }
 
 
 
+
     // Start is called before the first frame update
-    void Start(){
-		// Store reference to attached component
-		controller = GetComponent<CharacterController>();
-	}
+    void Start()
+    {
+        // Store reference to attached component
+        controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
