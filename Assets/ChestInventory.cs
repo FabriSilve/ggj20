89ef2c94f47731a673f41114881fa5c6ab1374f3
Daniele Riccardelli <dan.riccardelli@gmail.com>
2020-02-01 12:56:30 +0100

    Add Chest prefab with attached ChestInventory script

---
+++ Assets/ChestInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInventory : MonoBehaviour
+{
+    public List<Item> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int Weight() {
+        int weight = 0;
+        foreach (var item in items) {
+            weight += item.weight;
+        }
+        return weight;
+    }
+
+    int Size() {
+        return items.Count;
+    }
+
+    void Pick(Item item) {
+        items.Remove(item);
+    }
+}
+
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public int maxSize = 10;
+    private List<Item> storage = new List<Item>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,30 @@ public class PlayerInventory : MonoBehaviour
     void Update()
     {
         
+    }
+
+    int Weight() {
+        int weight = 0;
+        foreach (var item in storage) {
+            weight += item.weight;
+        }
+        return weight;
+    }
+
+    int Size() {
+        return storage.Count;
+    }
+
+    bool Insert(Item item) {
+        if (Size() == maxSize) {
+            return false;
+        } else {
+            storage.Add(item);
+            return true;
+        }
+    }
+
+    void Consume(Item item) {
+        storage.Remove(item);
     }
 }
