1ebdb2204d650e780176abfebcd32ec1de2fcea3
Abraham Georgiadis <abraham@manomotion.com>
2020-02-01 14:59:33 +0100

    We already spawn Cells and I am trying to have the water tile spawning in the LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,6 +4,60 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    //TODO Make the Application Manager use the methods here. 
+
+    [SerializeField]
+    GameObject baseTerrain;
+
+    [SerializeField]
+    GameObject spawnPointPrefab;
+
+    [SerializeField]
+    SpawnPoint[] allSpawnPointLogic;
+
+    int spawnPointXSize;
+    int spawnPointZSize;
+
+
+    Transform[] allLevelSpawnPoints;
+    void InitializeScales()
+    {
+        spawnPointXSize = (int)(baseTerrain.transform.localScale.x / spawnPointPrefab.transform.localScale.x);
+        spawnPointZSize = (int)(baseTerrain.transform.localScale.z / spawnPointPrefab.transform.localScale.z);
+
+        allLevelSpawnPoints = new Transform[spawnPointXSize * spawnPointZSize];
+    }
+
+
+    public void PopulateTerrain()
+    {
+        //TODO I should destroy any existing ones
+
+        InitializeScales();
+
+        for (int i = 0; i < spawnPointXSize; i++)
+        {
+            for (int y = 0; y < spawnPointZSize; y++)
+            {
+
+                GameObject spawnPoint = Instantiate(spawnPointPrefab);
+                spawnPoint.name = "SpawnPoint " + i + y;
+                allLevelSpawnPoints[i + y] = spawnPoint.transform;
+                allSpawnPointLogic[i] = spawnPoint.GetComponent<SpawnPoint>();
+
+                //I feel I have to do something more here
+                float x = -baseTerrain.transform.localScale.x / 2 + spawnPoint.transform.localScale.x / 2 + (i * spawnPoint.transform.localScale.x);
+                float z = baseTerrain.transform.localScale.x / 2 - spawnPoint.transform.localScale.x / 2 - (y * spawnPoint.transform.localScale.x);
+
+                spawnPoint.transform.position = new Vector3(x, 1, z);
+
+            }
+        }
+
+
+    }
+
+
     int timeDelay = 15;
     Transform[] allPoints;
     bool foundSpawnPoint = false;
@@ -27,17 +81,18 @@ public class LevelManager : MonoBehaviour
         PlayerPrefs.SetInt("Score", xpAmmount);
     }
 
-    void CalculateTimeInterval()
-    {
 
-    }
 
     void SpawnRandomWaterTile()
     {
-        while (!foundSpawnPoint)
+        int randompoint = Random.Range(0, allLevelSpawnPoints.Length);
+        if (!allSpawnPointLogic[randompoint].isOccupied)
         {
-
+            //Spawn a Water Tile
+            allSpawnPointLogic[randompoint].isOccupied = true;
         }
+
+
 
     }
 }
